416a4e73da5ad18b9ecf098c4d7e3bad3f8b734f
bahama <bahama@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-12 22:30:47 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/unfoldr@133 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/unfoldr/inst/examples/simCylinders.R
+++ pkg/unfoldr/inst/examples/simCylinders.R
@@ -14,43 +14,41 @@ col <- c("#0000FF","#00FF00","#FF0000","#FF00FF","#FFFF00","#00FFFF")
 ## intensity
 lam <- 500
 
-# simulation bounding box
+## simulation bounding box
 box <- list("xrange"=c(0,2),"yrange"=c(0,2),"zrange"=c(0,5))
 
-## show how to call
-#head(simPoissonSystem)
-
-.tA <- function(p,ctr,a,b,phi) {
-	(p[1]-ctr[1] + sin(phi)/cos(phi)*(p[2]-ctr[2]))/(a*cos(phi)+a*((sin(phi))^2)/cos(phi))
-}
-
-.PointOnEllipse <- function(ctr,a,b,phi,tt) {
-	pxy <- numeric(2)
-	pxy[1] = ctr[1] + a*cos(tt)*cos(phi)-b*sin(tt)*sin(phi)
-	pxy[2] = ctr[2] + a*cos(tt)*sin(phi)+b*sin(tt)*cos(phi)
-	return (pxy)
-}
-
-draw.EllipseSector <- function(x, ctr , nv=100,...) {
-	
-	psi0_1 <- ifelse(x$pS<0,-x$psi[1],x$psi[1])
-	psi1_1 <- ifelse(x$pS<0, x$psi[1],2*pi-x$psi[1])
-	
-	psi0_2 <- ifelse(x$pS>0,-x$psi[2],x$psi[2])
-	psi1_2 <- ifelse(x$pS>0, x$psi[2],2*pi-x$psi[2])
-	
-	tt <- sort(c(psi0_1,psi1_1,psi0_2,psi1_2))
-	
-	pp1 <- rbind(t(sapply(seq(tt[2],tt[3],length=nv), function(xi) .PointOnEllipse(ctr,x$ab[1],x$ab[2],x$phi,xi))))
-	pp2 <- rbind(t(sapply(seq(tt[4],2*pi-abs(tt[1]),length=nv), function(xi) .PointOnEllipse(ctr,x$ab[1],x$ab[2],x$phi,xi))))
-	
-	pp <- rbind(.PointOnEllipse(ctr,x$ab[1],x$ab[2],x$phi,tt[1]), pp1 ,pp2)
-	polygon(pp[,1], pp[,2],...)
-	
-}
-
+## drawing function of 2D intersection of spherocylinders
 draw.segments <- function(E, cyltype=0, x=c(0,1), y=x, angle=0, normal=c(0,0,1),axes=TRUE, ...)
 {
+	.tA <- function(p,ctr,a,b,phi) {
+		(p[1]-ctr[1] + sin(phi)/cos(phi)*(p[2]-ctr[2]))/(a*cos(phi)+a*((sin(phi))^2)/cos(phi))
+	}
+	
+	.PointOnEllipse <- function(ctr,a,b,phi,tt) {
+		pxy <- numeric(2)
+		pxy[1] = ctr[1] + a*cos(tt)*cos(phi)-b*sin(tt)*sin(phi)
+		pxy[2] = ctr[2] + a*cos(tt)*sin(phi)+b*sin(tt)*cos(phi)
+		return (pxy)
+	}
+	
+	.EllipseSector <- function(x, ctr , nv=100,...)
+	{
+		psi0_1 <- ifelse(x$pS<0,-x$psi[1],x$psi[1])
+		psi1_1 <- ifelse(x$pS<0, x$psi[1],2*pi-x$psi[1])
+		
+		psi0_2 <- ifelse(x$pS>0,-x$psi[2],x$psi[2])
+		psi1_2 <- ifelse(x$pS>0, x$psi[2],2*pi-x$psi[2])
+		
+		tt <- sort(c(psi0_1,psi1_1,psi0_2,psi1_2))
+		
+		pp1 <- rbind(t(sapply(seq(tt[2],tt[3],length=nv), function(xi) .PointOnEllipse(ctr,x$ab[1],x$ab[2],x$phi,xi))))
+		pp2 <- rbind(t(sapply(seq(tt[4],2*pi-abs(tt[1]),length=nv), function(xi) .PointOnEllipse(ctr,x$ab[1],x$ab[2],x$phi,xi))))
+		
+		pp <- rbind(.PointOnEllipse(ctr,x$ab[1],x$ab[2],x$phi,tt[1]), pp1 ,pp2)
+		polygon(pp[,1], pp[,2],...)	
+	}
+	
+	
 	argg <- c(as.list(environment()), list(...))
 	mycol <- "black"
 	if(!is.null(argg$col) && length(argg$col) > 0)
@@ -69,7 +67,7 @@ draw.segments <- function(E, cyltype=0, x=c(0,1), y=x, angle=0, normal=c(0,0,1),
 		X <- E[idType]
 		lapply(1:length(X), function(k) {
 					x <- X[[k]]
-					draw.EllipseSector(x,x$center[c(i,j)], ...)
+					.EllipseSector(x,x$center[c(i,j)], ...)
 					
 					## fist end cap
 					t <- x$psi[2]
@@ -87,6 +85,7 @@ draw.segments <- function(E, cyltype=0, x=c(0,1), y=x, angle=0, normal=c(0,0,1),
 					t0 <- acos(.tA(p,x$mPoint0[c(i,j)],x$rcaps[1],x$rcaps[1],x$phi))
 					psi0 <- ifelse(x$pS<0,-t0,t0)
 					psi1 <- ifelse(x$pS<0, t0,2*pi-t0)
+					
 					draw.ellipse(x$mPoint0[i],x$mPoint0[j], x$rcaps[1], x$rcaps[1],angle=x$phi,segment=c(psi0,psi1), deg=FALSE,...)
 						
 				})
@@ -134,8 +133,6 @@ draw.segments <- function(E, cyltype=0, x=c(0,1), y=x, angle=0, normal=c(0,0,1),
 		X <- E[idType]
 		lapply(1:length(X), function(k) {
 					x <- X[[k]]
-					#phi <- cartesian2sphere(xx$major)
-					
 					if(x$rcaps[1]>0) {
 						# plot circle
 						# get point on ellipse where to cut off and
@@ -148,13 +145,8 @@ draw.segments <- function(E, cyltype=0, x=c(0,1), y=x, angle=0, normal=c(0,0,1),
 						
 						draw.ellipse(x$mPoint0[i],x$mPoint0[j],x$rcaps[1],x$rcaps[1],
 								angle=x$phi,segment=c(psi0,psi1), deg=FALSE,...)
-						
-						#pxy <- .PointOnCircle(x$mPoint0,x$rcaps[1],x$phi,ifelse(x$pS<0,0,pi))
-						#points(pxy[1],pxy[2],col="red")
-						#segments(x$ipt0[1],x$ipt0[2], pxy[1],pxy[2],col="red")
-						
+		
 					}
-
 				})
 	}	
 }
@@ -178,16 +170,18 @@ cylinders3d(S[1:1000], draw.box=TRUE, box=box, col=col)
 
 
 #################################################################
-## simulate bivariate
+## simulate bivariate size/shape distribution
 #################################################################
 
-## no `shape` required
+## no extra `shape` factor required, see documentation
 theta <- list("size"=list("mx"=-1.45,"my"=-2.0, "sdx"=0.15,"sdy"=0.25,"rho"=0.0),
 			  "orientation"=list("kappa"=5))
 
 S <- simPoissonSystem(theta,lam,size="rbinorm",box=box,type="cylinders",
 		intersect="full", n=c(0,0,1), "orientation"="rbetaiso", dz=2.5,
 		perfect=TRUE,intern=FALSE,delta=0.005, pl=101)
+
+## show the full system
 
 #open3d()
 #cylinders3d(S$S[1:1000], draw.box=TRUE, box=box, col=col)
@@ -230,70 +224,60 @@ W2 <- digitizeProfiles(SP, delta=0.005)
 dev.new()
 image(1:nrow(W2),1:ncol(W2),W2,col=gray(1:0))
 
-
 #################################################################
 ## Update intersection: find objects which intersect bounding box
 #################################################################
 
 idx <- updateIntersections(S)
-sum(!idx)							# objects intersecting
+sum(!idx)							# number of objects intersecting bounding box
 id <- which( idx != 1)	
 
 # show in 3D
 open3d()
-spheroids3d(S[id], FALSE, TRUE, box=box, col=col)
+cylinders3d(S[id], draw.box=TRUE, box=box, col=col)
+
 
 #################################################################
-## user-defined simulation function
+## example of a user-defined distributino of size and shape
 #################################################################
 
-
-# no perfect simualtion here for 'rmulti'
-# multivariate size distribution,
+# no perfect simualtion here:
+# log normal length of cylinder, beta distributed shape factor,
 # independent orientation distribution 
-rmulti <- function(m,s,kappa) {	
-	# directional distribution
-	# (implemented `rbetaiso` distribution)
-	rbetaiso <- function(kappa) {
-		phi <- runif(1,0,1)*2*pi
-		q <- runif(1,0,1)
-		theta=acos((1-2*q)/sqrt(kappa*kappa-(1-2*q)*(1-2*q)*(kappa*kappa-1)))
-		list("u"=c(sin(theta)*cos(phi),sin(theta)*sin(phi),cos(theta)),
-				"theta"=theta,"phi"=phi)					
-	}
+rmulti <- function(m,s) {	
+	# directional distribution	
+	phi <- runif(1)*2*pi
+	theta <- acos(2.0*runif(1)-1.0)
+	dir <- list("u"=c(cos(phi)*sin(theta),sin(phi)*sin(theta),cos(theta)),
+			    "theta"=theta,"phi"=phi)
+	len <- rlnorm(1,m,s)
+	shape <- rbeta(1,1,10)
+	r <- len*shape
 	
-	dir <- rbetaiso(kappa)
-	# log normal semi-major/semi-minor lengths
-	M <- chol(s, pivot = TRUE)
-	M <- M[,order(attr(M, "pivot"))]
-	x <- exp(matrix(m,nrow=1) + matrix(rnorm(ncol(s)), nrow = 1, byrow = TRUE) %*% M)
-	a <- min(x)
-	b <- max(x)
-	# the following elements are obligatory as a
-	# return value for user-defined spheroid simulations
-	list("a"=a,"b"=b,"c"=a,"u"=dir$u,"shape"=a/b,"theta"=dir$theta, "phi"=dir$phi)	
+	# the following elements are obligatory as return values within a list
+	list("h"=len-2*r,"r"=r,"u"=dir$u,"shape"=shape,"theta"=dir$theta, "phi"=dir$phi)	
 }
 
-
-sigma <- matrix(c(0.1,0.1,0.1,0.25), ncol=2)
-theta <- list("m"=c(-3.0,-2.0),"s"=sigma,"kappa"=0.5)
-
-S <- simPoissonSystem(theta,lam,rjoint=rmulti,box=box,type="prolate",
+# parameter for size distribution
+theta <- list("m"=-2.25,"s"=0.35)
+# simulate system
+S <- simPoissonSystem(theta,lam,rjoint=rmulti,box=box,type="cylinders",
 		intersect="full",n=c(0,0,1), mu=c(0,0,1), dz=2.5, pl=101)
 
 # in 3D
 sp <- S$sp # sections
-id <- sapply(sp,"[[","id") 
+id <- sapply(sp,"[[","id") # indices of intersected objects 
+# show
 open3d()
-spheroids3d(S$S[id], FALSE, TRUE, box=box, col=col)
+cylinders3d(S$S[id], draw.box=TRUE, box=box, col=col)
 planes3d(0,0,-1,2.5,col="black",alpha=1)
 
-# in 2D
+# 2D intersection profiles
 dev.new()
-Es <- drawEllipses(sp, x=box$xrange, border="black",xlab="[mm]", ylab="[mm]",
-		rot=0, bg="gray",col=col,	cex.lab=1.8,cex=1.8,cex.axis=1.8,nv=1000)
+draw.segments(sp,x=win[[1]],y=win[[2]],bg="gray",normal=c(0,0,1),xlab="[mm]",
+	xaxs="i",yaxs="i",ylab="[mm]",col="black",axes=TRUE,cex.lab=1.8,cex=1.8,cex.axis=1.8)
 
-# digitized
-W <- S$W
+## digitize objects
+W <- digitizeProfiles(sp, delta=0.001)
 dev.new()
 image(1:nrow(W),1:ncol(W),W,col=gray(1:0))
