55a9d73ab10390ece20c4c4d6bc0b578283e573e
bahama <bahama@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-22 13:35:55 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/unfoldr@212 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/unfoldr/inst/examples/coeffarray.R
+++ pkg/unfoldr/inst/examples/coeffarray.R
@@ -1,5 +1,7 @@
 \dontrun{
-
+## Comment: Set breaks vector and compute the coefficient matrix 
+## for spheroid unfolding
+	
 options(par.unfoldr=2L)
 breaks <- setbreaks(c(6,5,6),maxSize=0.37,kap=1.25)
 breaks
